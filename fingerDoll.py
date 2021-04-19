e85ce9872f564675365ba6db0245ca9681aa92a5
Abdolhosein V. Ebrahimi <vakilzadeh@gmail.com>
2021-04-19 15:13:45 +0430

    update : now it's displaying dolls on fingers

--- fingerDoll.py
+++ fingerDoll.py
@@ -7,22 +7,25 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(min_detection_confidence=0.6, max_num_hands=1)
+hands = mpHands.Hands(min_detection_confidence=0.7, max_num_hands=2)
 mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
+sib, sibW, sibH = [None] * 5, [None] * 5, [None] * 5
+for s in range(0, 5):
+    print(f"reading image {s}")
+    sib[s] = cv2.imread(f'd{5-s}.png', cv2.IMREAD_UNCHANGED)
+    scale_percent = 65  # percent of original size
+    width = int(sib[s].shape[1] * scale_percent / 100)
+    height = int(sib[s].shape[0] * scale_percent / 100)
+    dim = (width, height)
+    sib[s] = cv2.resize(sib[s], dim, interpolation=cv2.INTER_AREA)
+    BLUE = [255, 255, 255]
+    sib[s] = cv2.copyMakeBorder(sib[s], 64, 64, 64, 64, cv2.BORDER_CONSTANT, value=BLUE)
+    sibW[s], sibH[s], sibC = sib[s].shape
 
-sib = cv2.imread('d1.png', cv2.IMREAD_UNCHANGED)
-scale_percent = 65  # percent of original size
-width = int(sib.shape[1] * scale_percent / 100)
-height = int(sib.shape[0] * scale_percent / 100)
-dim = (width, height)
-sib = cv2.resize(sib, dim, interpolation=cv2.INTER_AREA)
-BLUE = [255, 255, 255]
-sib = cv2.copyMakeBorder(sib, 64, 64, 64, 64, cv2.BORDER_CONSTANT, value=BLUE)
-
-sibW, sibH, sibC = sib.shape
+fingerTips = [4, 8, 12, 16, 20]
 
 
 def overlay_transparent(background, overlay, x, y):
@@ -60,16 +63,13 @@ def overlay_transparent(background, overlay, x, y):
 
 
 def angle_of_line(x1, y1, x2, y2):
-    return math.degrees(math.atan2(-y2+y1, x2 - x1))
+    return math.degrees(math.atan2(-y2 + y1, x2 - x1))
 
 
 def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    maxSide = int(3.4*max(image.shape[0],image.shape[1]))
-    print(image.shape)
-    print(maxSide)
-    result = cv2.warpAffine(image, rot_mat, (maxSide,maxSide), flags=cv2.INTER_LINEAR)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
 
 
@@ -84,21 +84,22 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                if id in [4, 8, 12, 16, 20]:
+                if id in fingerTips:
+                    finger = fingerTips.index(id)
                     lm_prev = handLms.landmark[id - 1]
                     px, py = int(lm_prev.x * w), int(lm_prev.y * h)
-                    angleOfRotation = angle_of_line(cx, cy, px, py)+90
+                    angleOfRotation = angle_of_line(cx, cy, px, py) + 90
                     # print(cx, cy, px, py)
                     # print(angleOfRotation)
-                    rImg = rotate_image(sib, angleOfRotation)
-                    tx, ty = int(cx - sibW / 2), int(cy - sibH / 2)
+                    rImg = rotate_image(sib[finger], angleOfRotation)
+                    tx, ty = int(cx - sibW[finger] / 2), int(cy - sibH[finger] / 2)
                     tx = max(0, tx)
                     ty = max(0, ty)
                     overlay_transparent(img, rImg, tx, ty)
-                if id > 0:
-                    cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
-                    cv2.putText(img, str(id), (cx, cy), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 2)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                # if id > 0:
+                    # cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+                    # cv2.putText(img, str(id), (cx, cy), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 2)
+            # mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
